1d5fa01ea8d949b37005c293fc2d3b20d2907388
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2019-04-02 20:32:28 +0100

    changed the logic so that now it is possible to reach maximum zoom values

--- constants.lua
+++ constants.lua
@@ -1,5 +1,5 @@
 return {
-    MAX_WORLD_ZOOM_IN_LEVEL = 20,
+    MAX_WORLD_ZOOM_IN_LEVEL = 10,
     MAX_MAP_ZOOM_IN_LEVEL = 1,
     MAX_MAP_ZOOM_OUT_LEVEL = 0.0001, --TODO should this be configurable or should specific actions (like insta zoom out) have their own configurations? ANSWER: insta zoom out should have their own config
     BASE_GAME_PLAYER_ZOOM_SENSITIVITY = 1.1,
--- zoom_calculator.lua
+++ zoom_calculator.lua
@@ -15,14 +15,11 @@ function calculator.calculate_zoomed_in_level(player)
         max_zoom_in_level = constant.MAX_MAP_ZOOM_IN_LEVEL
     end
 
-    --TODO for now, I guess I could just set it to the actual max rather than ignoring it
-    -- it will simplify things when watching when the game switched to zoom to world by using base game's "alternative zoom in" on the map
-    -- in the future, on mod settings change I could calculate what is the actual min/max and store that in player's memory
-    --TODO but then, what to do with features like quick zoom out which will have its zoom level configurable?
-    -- I would have to recalculate it and set it to the closest "correct" value...
-    if new_zoom_level <= max_zoom_in_level then
-        player_memory.set_current_zoom_level(player, new_zoom_level)
+    if new_zoom_level > max_zoom_in_level then
+        new_zoom_level = max_zoom_in_level
     end
+
+    player_memory.set_current_zoom_level(player, new_zoom_level)
 
     return player_memory.get_current_zoom_level(player)
 end
@@ -40,18 +37,19 @@ function calculator.calculate_zoomed_out_level(player)
         max_zoom_out_level = constant.MAX_MAP_ZOOM_OUT_LEVEL
     end
 
-    if new_zoom_level >= max_zoom_out_level then
-        player_memory.set_current_zoom_level(player, new_zoom_level)
+    if new_zoom_level < max_zoom_out_level then
+        new_zoom_level = max_zoom_out_level
     end
+
+    player_memory.set_current_zoom_level(player, new_zoom_level)
 
     return player_memory.get_current_zoom_level(player)
 end
 
 function calculator.is_maximally_zoomed_out_zoom_to_world_view(player)
     if player.render_mode == defines.render_mode.chart_zoomed_in then
-        local zoom_sensitivity = player.mod_settings["ZoomingReinvented_zoom-sensitivity"].value
         local max_world_zoom_out_level = player.mod_settings["ZoomingReinvented_max-world-zoom-out"].value
-        return player_memory.get_current_zoom_level(player) / zoom_sensitivity < max_world_zoom_out_level
+        return player_memory.get_current_zoom_level(player) == max_world_zoom_out_level
     else
         return false
     end
