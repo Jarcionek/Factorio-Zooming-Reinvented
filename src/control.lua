109dd20532345affb6be73870385674209fc5954
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2019-04-06 19:45:12 +0100

    quick zoom out does no longer reset last_known_map_position

--- src/control.lua
+++ src/control.lua
@@ -92,8 +92,8 @@ script.on_event("ZoomingReinvented_quick-zoom-out", function(event)
 
     player.open_map({0, 0}, zoom_level)
 
+    -- do not reset last_known_map_position to allow to use quick-zoom-in to go back to it
     player_memory.set_current_zoom_level(player, zoom_level)
-    player_memory.set_last_known_map_position(player, {0, 0})
     map_zoom_out_disabler.enable(player)
 end)
 
