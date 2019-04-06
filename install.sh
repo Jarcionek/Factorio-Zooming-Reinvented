8c22e04c136d13b20a5a4a19a42441286eb4086b
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2019-04-06 20:37:23 +0100

    prepared release 1.2.0

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 # prepare zip file
-mkdir ZoomingReinvented_1.1.0
-cp README.md ZoomingReinvented_1.1.0
-cp LICENSE ZoomingReinvented_1.1.0
-cp changelog.txt ZoomingReinvented_1.1.0
-cp -R src/* ZoomingReinvented_1.1.0
-zip -r ZoomingReinvented_1.1.0{.zip,}
+mkdir ZoomingReinvented_1.2.0
+cp README.md ZoomingReinvented_1.2.0
+cp LICENSE ZoomingReinvented_1.2.0
+cp changelog.txt ZoomingReinvented_1.2.0
+cp -R src/* ZoomingReinvented_1.2.0
+zip -r ZoomingReinvented_1.2.0{.zip,}
 
 # move zip to factorio mods folder (overriding existing one if present)
-mv ZoomingReinvented_1.1.0.zip "/cygdrive/c/Users/${USER}/AppData/Roaming/Factorio/mods"
+mv ZoomingReinvented_1.2.0.zip "/cygdrive/c/Users/${USER}/AppData/Roaming/Factorio/mods"
 
 # cleanup
-rm -r ZoomingReinvented_1.1.0
+rm -r ZoomingReinvented_1.2.0
